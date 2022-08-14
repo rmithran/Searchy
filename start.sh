0c734a9c9d6777bcf6b4b9b630cd002e07412e21
rmithran <rmithran0311@gmail.com>
2022-08-14 12:13:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kajee2002/Searchy.git /Searchy
+  git clone https://github.com/rmithran/Searchy.git /Searchy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Searchy
